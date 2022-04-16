effb0e0f6676ae4edc20a663359447605c4ac950
David Stobbe <d.stobbe1@share.epsb.ca>
2022-04-16 20:29:59 +0000

    Done jumping, score, animation

---
+++ script.js
@@ -0,0 +1,119 @@
+// Dinosaur Game - David Stobbe
+
+let cnv = document.getElementById("canvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 700;
+cnv.height = 200;
+let cactusImg = document.getElementById("cactus");
+let dinosaurImg = document.getElementById("dinosaur");
+
+let cactusDisplayInterval = Math.round(Math.random() * 200);
+let cactusDisplayDuration = 0;
+let cacti = []
+let dinosaurY = 70
+let jumping = false
+let jumpTimer = 0
+let gravity = 0.4
+let death = false;
+let score = 0
+let highScore = 0
+let dinosaurX = 80
+let dinosaurW = 30
+let dinosaurH = 30
+
+
+document.addEventListener("keydown", keydownListener)
+
+class cactus {
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+    display() {
+        ctx.drawImage(cactusImg, this.x, this.y, this.w, this.h)
+        this.x -= 4
+
+        if (dinosaurX + dinosaurW - 6 >= this.x && dinosaurX + dinosaurW - 6 <= this.x + 18 && dinosaurY + dinosaurH - 6 >= this.y && dinosaurY + dinosaurH - 6 <= this.y + 30) {
+            death = true
+        }
+    }
+}
+
+
+requestAnimationFrame(loop)
+function loop() { 
+    if (!death) {
+        console.log(dinosaurY)
+        console.log(jumping)
+        ctx.fillStyle = "rgb(255, 255, 255)"
+        ctx.fillRect(0, 0, cnv.width, cnv.height)
+        ctx.font = "20px Sabo"
+        ctx.fillStyle = "rgb(0, 0, 0)"
+        ctx.fillText("Score: " + score, 600, 20)
+        ctx.fillText("High Score: " + highScore, 400, 20)
+        if(jumping === true) {
+            jumpTimer += 0.7
+        }
+        console.log(jumpTimer)
+        console.log(jumping)
+        ctx.drawImage(dinosaurImg, dinosaurX, dinosaurY, dinosaurW, dinosaurH)
+        cactusDisplayDuration ++;
+        if (cactusDisplayDuration === cactusDisplayInterval) {
+            cacti.push(new cactus(cnv.width, 70, 20, 30));
+            cactusDisplayDuration = 0;
+            cactusDisplayInterval = Math.round(Math.random() * 200);
+        }
+        if (jumping === true) {
+            dinosaurY = (0.5 * gravity * jumpTimer * jumpTimer - 6 * jumpTimer + 70)
+            if (dinosaurY > 70) {
+                dinosaurY = 70
+                jumping = false;
+            }
+        }
+
+        for (let n = 0; n < cacti.length; n++) {
+            cacti[n].display();
+            if (cacti[n].x <= -20) {
+                cacti.splice(n, 1)
+            }
+        }
+
+    }
+    else {
+        ctx.fillText("Press the space bar to restart game", 250, 180)
+    }
+    requestAnimationFrame(loop)
+}
+
+function keydownListener(event) {
+    if (event.code === "Space") {
+        if (jumping === false) {
+            jumping = true;
+            jumpTimer = 0
+        }
+        if (death) {
+            if (score > highScore) {
+                highScore = score;
+            }
+            score = 0
+            cacti.splice(0, cacti.length)
+            death = false;
+        }
+    }
+}
+
+function Gamescore() {
+    if (!death) {
+        score++
+        if (!jumping) {
+            if (dinosaurImg.src === "http://127.0.0.1:5500/img/dino1.png") {
+                dinosaurImg.src = "img/dino2.png"
+            } else {
+                dinosaurImg.src = "img/dino1.png"
+            }
+        }
+    }
+}
+setInterval(Gamescore, 100)
